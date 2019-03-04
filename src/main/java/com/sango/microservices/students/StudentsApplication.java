fea403b0faa1587ebf68980cd0d2955b23d4b433
nita <nita@nitas.com>
2019-03-04 15:14:16 -0500

    Added the Eureka Naming server dependency for Discovery Client

--- src/main/java/com/sango/microservices/students/StudentsApplication.java
+++ src/main/java/com/sango/microservices/students/StudentsApplication.java
@@ -2,8 +2,10 @@ package com.sango.microservices.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister=true)
 public class StudentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);

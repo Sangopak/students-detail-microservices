b92d967747c6260fc1c3c0dd7ab082ab9815569f
nita <nita@nitas.com>
2019-07-18 18:51:14 -0400

    Added lombok for clearing boiler plate code

--- src/main/java/com/sango/microservices/students/controller/StudentController.java
+++ src/main/java/com/sango/microservices/students/controller/StudentController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,14 +24,13 @@ import com.sango.microservices.students.exception.StudentNotFoundException;
 import com.sango.microservices.students.repository.StudentRepository;
 import com.sango.microservices.students.util.CommonConstant;
 
+@Slf4j
 @RestController
 @RequestMapping(path="/api/")
 public class StudentController {
-	
-	private static final Logger log = LoggerFactory.getLogger(StudentController.class);
-	
+
 	@Autowired
-	StudentRepository studentRepository;
+	private StudentRepository studentRepository;
 	
 	@GetMapping(path="/students", produces="application/json")
 	public List<Student> getAllStudents(){
--- src/main/java/com/sango/microservices/students/domain/Student.java
+++ src/main/java/com/sango/microservices/students/domain/Student.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(value="students")
 public class Student implements Serializable{
 	
@@ -29,97 +37,5 @@ public class Student implements Serializable{
 	private String state;
 	private String zip;
 	private String course;
-	
-	public Student() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Student(UUID id, String name, Date dob, String street, String street2, String city, String state,
-			String zip, String course) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.dob = dob;
-		this.street = street;
-		this.street2 = street2;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.course = course;
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Date getDob() {
-		return dob;
-	}
-
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getStreet2() {
-		return street2;
-	}
-
-	public void setStreet2(String street2) {
-		this.street2 = street2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	public String getCourse() {
-		return course;
-	}
-
-	public void setCourse(String course) {
-		this.course = course;
-	}
-
-	
 
 }

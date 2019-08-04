3d4cd5e1e9dd59f383968e8c4fde8a017287dee7
nita <nita@nitas.com>
2019-08-04 00:53:11 -0400

    Removed password hardcoding

--- src/main/java/com/sango/microservices/students/config/OauthResourceConfiguration.java
+++ src/main/java/com/sango/microservices/students/config/OauthResourceConfiguration.java
@@ -1,5 +1,6 @@
 package com.sango.microservices.students.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
@@ -13,12 +14,22 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OauthResourceConfiguration extends GlobalMethodSecurityConfiguration {
+
+    @Value("${spring.security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.check-token-endpoint-url}")
+    private String checkTokenEndpointUrl;
+
     @Bean
     public RemoteTokenServices tokenServices() {
         RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
-        remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:9090/spring-security-oauth-server/oauth/check_token");
-        remoteTokenServices.setClientId("studentProtectedResourceClientIdPassword");
-        remoteTokenServices.setClientSecret("secret");
+        remoteTokenServices.setCheckTokenEndpointUrl(checkTokenEndpointUrl);
+        remoteTokenServices.setClientId(clientId);
+        remoteTokenServices.setClientSecret(clientSecret);
         return remoteTokenServices;
     }
 
